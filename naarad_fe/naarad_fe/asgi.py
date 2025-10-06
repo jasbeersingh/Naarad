13cdf71e5a4027676214448dee0be9917e72a978
jasbeersingh <jasbeer.gaingyaan@gmail.com>
2025-10-06 11:43:42 +0530

    added base structure to the project

---
+++ naarad_fe/naarad_fe/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for naarad_fe project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'naarad_fe.settings')
+
+application = get_asgi_application()
